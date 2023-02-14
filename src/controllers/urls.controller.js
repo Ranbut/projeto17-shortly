64085a3e8d5c07abe9d5ad8e35ffba1bbf06947e
Ranbut <Ranbut_Beater@protonmail.com>
2023-02-14 17:39:50 -0300

    feat: /urls DELETE

--- src/controllers/urls.controller.js
+++ src/controllers/urls.controller.js
@@ -5,11 +5,18 @@ import dayjs from "dayjs";
 export async function CreateUrl (req, res) {
     const { url } = req.body;
 
+    const authorization = req.headers.authorization;
+
     try{
+
+        const header = authorization.split(' ');
+        const bearer = header[1];
+        const userId = (await db.query(`SELECT id FROM sessions WHERE token='${bearer}';`)).rows[0].id;
+
         let urlKey = nanoid(8);
         const createdAt = dayjs().format("YYYY-MM-DD");
-
-        await db.query(`INSERT INTO "shortUrls" ("shortUrl", url, "createdAt") VALUES ('${urlKey}', '${url}', '${createdAt}');`);
+        
+        await db.query(`INSERT INTO "shortUrls" ("shortUrl", url, "createdAt", "userId") VALUES ('${urlKey}', '${url}', '${createdAt}', ${userId});`);
         const createId = (await db.query(`SELECT * FROM "shortUrls" ORDER BY ID DESC LIMIT 1;`)).rows[0].id;
         res.status(201).send({id: createId, shortUrl: urlKey});
     }
@@ -52,10 +59,24 @@ export async function OpenShortUrl (req, res) {
 
 export async function DeleteUrl (req, res) {
 
-    const { id } = req.query;
+    const { id } = req.params;
+    const authorization = req.headers.authorization;
+
+    const header = authorization.split(' ');
+    const bearer = header[1];
 
     try{
+        const tokenQuery = await db.query(`SELECT token FROM sessions WHERE token='${bearer}';`);
 
+        if (!authorization || tokenQuery.rowCount === 0) return res.sendStatus(401);
+
+        const shortUrl = (await db.query(`SELECT * FROM "shortUrls" WHERE id = ${id}`)).rows[0];
+
+        if (!shortUrl) return res.sendStatus(404);
+
+        await db.query(`DELETE FROM "shortUrls" WHERE id=${id}`);
+
+        res.sendStatus(200);
     }
     
     catch(err){
